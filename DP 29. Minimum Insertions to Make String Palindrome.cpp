b4393beacd930080f78e54433181004283080838
soham6125 <chaudhuri.ber@gmail.com>
2022-05-22 13:46:43 +0530

    UPDATE

---
+++ DP 29. Minimum Insertions to Make String Palindrome.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+int minInsertion(string &s)
+{
+    int n = s.length();
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    for (int i = 0; i < n; i++)
+        dp[i][i] = 1;
+    for (int len = 2; len <= n; len++)
+    {
+        for (int i = 0; i < n - len + 1; i++)
+        {
+            int j = i + len - 1;
+            if (s[i] == s[j] and len == 2)
+                dp[i][j] = 2;
+            else if (s[i] == s[j])
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            else
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+    // total length - (longest palindromic subsequence)
+    return n - dp[0][n - 1];
+}
---
+++ DP 30. Minimum insert-delete to Convert String A to String B.cpp
@@ -0,0 +1,26 @@
+// variation of edit distance problem
+// only difference is that here we can't change a character, only delete and insert one
+
+int canYouMake(string &word1, string &word2)
+{
+    int n = word1.size(), m = word2.size();
+    int dp[n + 1][m + 1] = {0};
+
+    // if one string is empty, we will insert all characters of other string
+    for (int i = 0; i < n + 1; i++)
+        dp[i][0] = i;
+    for (int j = 0; j < m + 1; j++)
+        dp[0][j] = j;
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < m + 1; j++)
+        {
+            if (word1[i - 1] == word2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[n][m];
+}
